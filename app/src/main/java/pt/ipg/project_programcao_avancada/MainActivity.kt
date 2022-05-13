68c46891066c6915926644208a57ebecbd15a959
vasco <vasco144oliveira@gmail.com>
2022-05-13 09:36:24 +0100

    criacao do projeto

---
+++ app/src/main/java/pt/ipg/project_programcao_avancada/MainActivity.kt
@@ -0,0 +1,13 @@
+package pt.ipg.project_programcao_avancada
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        println("Hello_world")
+    }
+}
